862260d0010b47790897cbbb1132a9fcb82cbe44
Adriano Díaz Benítez <64199230+PublioElio@users.noreply.github.com>
2022-12-21 23:08:04 +0100

    Add files via upload

--- bingo/src/bingo/Bingo.java
+++ bingo/src/bingo/Bingo.java
@@ -46,7 +46,8 @@ public class Bingo {
                         + "\n");
             }
 
-            for (int pos = 0; pos < array[indice].length; pos++) {
+            for (int pos = 0; (pos < array[indice].length)
+                    && (array[indice][pos] != 0); pos++) {
                 if (array[indice][pos] < 10) {
                     System.out.print(" | 0" + array[indice][pos] + " | ");
                 } else {
@@ -78,34 +79,43 @@ public class Bingo {
     static boolean estaRepetido(int[][] cartones, int jugador, int indice,
             int clave) {
         boolean repetido = false;
-        for (int i = 0; i < cartones[jugador].length && !repetido && i < indice;
-                i++) {
+        for (int i = 0; (i < cartones[jugador].length) && !repetido
+                && (i < indice); i++) {
             repetido = cartones[jugador][i] == clave;
         }
         return (repetido);
     }
 
-    static void jugarBingo(int[][] combGanadora, int[][] jugadores) {
+    static int jugarBingo(int[][] combGanadora, int[][] jugadores) {
         int num, totalNum = 0, ganador = 0;
-        boolean bingo = false;
+        boolean bingo = false, linea = false;
         while (!bingo) {
 
+            /* este do-while es el equivalente a "sacar un número del bombo" */
             do {
                 num = generarNumAleatorio(1, 90);
             } while (estaRepetido(combGanadora, 0, totalNum,
                     num));
             combGanadora[0][totalNum++] = num;
 
-            /* Empezamos a comprobar los cartones a partir de cinco números en
-            la combinación ganadora, porque antes es imposible cantar línea */
+            /* Empezamos a comprobar los cartones a partir de los cinco primeros
+            números, porque antes es imposible cantar línea */
             if (totalNum >= 5) {
+
                 int contador;
+
                 for (int jugador = 0; jugador < jugadores.length; jugador++) {
                     contador = 0;
                     for (int pos = 0; pos < jugadores[jugador].length; pos++) {
-                        for (int numPremiado = 0; numPremiado < combGanadora[0].length; numPremiado++) {
+                        for (int numPremiado = 0; (numPremiado < combGanadora[0].length) && (numPremiado <= totalNum); numPremiado++) {
                             if (jugadores[jugador][pos] == combGanadora[0][numPremiado]) {
                                 contador++;
+                            }
+                            if ((contador >= 5) && comprobarLineas(combGanadora, jugadores, totalNum, jugador) && !linea) {
+                                linea = true;
+                                System.out.println("\n¡El jugador "
+                                        + (jugador + 1)
+                                        + " ha cantado línea!\n");
                             }
                         }
                     }
@@ -116,18 +126,31 @@ public class Bingo {
                 }
             }
         }
-        System.out.println("GANADOR: " + (ganador + 1));
-        mostrarArray(combGanadora, 0, combGanadora.length,
-                "Números extraidos del bombo: ", false);
+        return (ganador);
+    }
 
+    static boolean comprobarLineas(int[][] combGanadora, int[][] jugadores, int indiceGanador, int jugador) {
+        boolean cantarLinea = false;
+        int recorrido = 0, limite = 4;
+        /* este while hace que se comprueben las posiciones de cinco en cinco */
+        while (!cantarLinea && (limite < 15)) {
+            /* este otro while comprueba cada una de las posiciones dentro de
+            una línea con la totalidad de los números de la 
+            combinación ganadora */
+            while ((recorrido <= limite) && (recorrido <= indiceGanador)) {
+                cantarLinea = estaRepetido(combGanadora, 0, indiceGanador, jugadores[jugador][recorrido]);
+                recorrido++;
+            }
+            limite += 5;
+        }
+        return (cantarLinea);
     }
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        int numJugadores;
-        numJugadores = obtenerEntero(4,
+        int numJugadores = obtenerEntero(4,
                 "Introduzca el número de jugadores (mínimo cuatro): ",
                 "ERROR: el número mínimo de jugadores es cuatro. ");
 
@@ -136,8 +159,9 @@ public class Bingo {
         mostrarArray(cartones, 0, cartones.length,
                 "JUGADOR: ", true);
         int[][] combGanadora = new int[1][90];
-        jugarBingo(combGanadora, cartones);
-
+        System.out.println("GANADOR: el jugador " + (jugarBingo(combGanadora, cartones) + 1));
+        mostrarArray(combGanadora, 0, combGanadora.length,
+                "Números extraidos del bombo: ", false);
     }
 
 }
